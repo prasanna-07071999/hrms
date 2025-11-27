3c448b8979d11bb35634df7911561a621fb1087b
Prasannakumar <bogachandrapu@gmail.com>
2025-11-27 13:02:33 +0530

    Solved backend error while login

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,30 @@
 require('dotenv').config()
-const Organisation = require('./models/organisation');
-const User = require('./models/user');
-const Employee = require('./models/employee');
-const Team = require('./models/team');
-const EmployeeTeam = require('./models/employeeTeam');
-const Log = require('./models/log');
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
-const authRoutes = require('./routes/auth');
-const { logMiddleware } = require('./middlewares/logMiddleware');
-const teamRoutes = require('./routes/teams');
-const employeeRoutes = require('./routes/employees');
-const logRoutes = require('./routes/logRoutes');
-const statRoute = require('./routes/stats');
+
+const {sequelize} = require('./db')
+const Organisation = require('./models/organisation')
+const User = require('./models/user')
+const Employee = require('./models/employee')
+const Team = require('./models/team')
+const EmployeeTeam = require('./models/employeeTeam')
+const Log = require('./models/log')
 const errorHandler = require("./middlewares/errorHandler");
-const authMiddleware = require('./middlewares/authMiddleware');
+const authMiddleware = require('./middlewares/authMiddleware')
+
+const seedData = require('./seed');
 const cors = require("cors");
 
+const authRoutes = require('./routes/auth')
+const teamRoutes = require('./routes/teams')
+const employeeRoutes = require('./routes/employees');
+const {logMiddleware} = require('./middlewares/logMiddleware');
+const logRoutes = require('./routes/logRoutes');
+const statRoute = require('./routes/stats')
+
 app.use(express.json());
+
 app.use(cors({
     origin: "https://hrms-z5xo.onrender.com",
     methods: "GET,POST,PUT,DELETE",
@@ -29,16 +35,14 @@ app.use('/api/auth', authRoutes);
 
 app.use(logMiddleware);
 
-app.use(authMiddleware);
+app.use(authMiddleware);  
 
 app.use('/api/teams', teamRoutes);
 app.use('/api/employees', employeeRoutes);
 app.use('/api/stats', statRoute);
-app.use('/api/logs', logRoutes);
+app.use('/api/logs', logRoutes); 
 
 app.use(errorHandler);
-
-module.exports = app;
 
 Organisation.hasMany(User, {foreignKey: 'organisationId'})
 User.belongsTo(Organisation, {foreignKey: 'organisationId'})
