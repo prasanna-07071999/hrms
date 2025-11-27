e842f1c1fe6768334f521fc65d9aa3ad30b62570
Prasannakumar <bogachandrapu@gmail.com>
2025-11-27 12:51:31 +0530

    Solved backend error while login

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,36 +2,34 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
-
+const authRoutes = require('./routes/auth');
+const { logMiddleware } = require('./middlewares/logMiddleware');
+const teamRoutes = require('./routes/teams');
+const employeeRoutes = require('./routes/employees');
+const logRoutes = require('./routes/logRoutes');
+const statRoute = require('./routes/stats');
+const errorHandler = require("./middlewares/errorHandler");
+const authMiddleware = require('./middlewares/authMiddleware');
 const cors = require("cors");
-app.use(express.json());
 
+app.use(express.json());
 app.use(cors({
     origin: "https://hrms-z5xo.onrender.com",
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization"
 }));
 
-const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
 
-const { logMiddleware } = require('./middlewares/logMiddleware');
 app.use(logMiddleware);
 
-const authMiddleware = require('./middlewares/authMiddleware');
 app.use(authMiddleware);
-
-const teamRoutes = require('./routes/teams');
-const employeeRoutes = require('./routes/employees');
-const logRoutes = require('./routes/logRoutes');
-const statRoute = require('./routes/stats');
 
 app.use('/api/teams', teamRoutes);
 app.use('/api/employees', employeeRoutes);
 app.use('/api/stats', statRoute);
 app.use('/api/logs', logRoutes);
 
-const errorHandler = require("./middlewares/errorHandler");
 app.use(errorHandler);
 
 module.exports = app;
